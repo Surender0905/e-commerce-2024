6b06e6929087cd0ec346642fdcc55f86204712b3
Surender Pratap Singh <rk31151@gmail.com>
2024-11-12 23:58:07 +0530

    fixed

--- frontend/src/components/FeaturedProducts.jsx
+++ frontend/src/components/FeaturedProducts.jsx
@@ -68,7 +68,7 @@ const FeaturedProducts = ({ featuredProducts }) => {
                                                 {product.name}
                                             </h3>
                                             <p className="text-emerald-300 font-medium mb-4">
-                                                ${product.price.toFixed(2)}
+                                                Rs {product.price.toFixed(2)}
                                             </p>
                                             <button
                                                 onClick={() =>
